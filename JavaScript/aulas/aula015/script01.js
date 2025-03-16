8b75302610ec8bcd5568590faea5c8834e8071e1
Ygor Victor Santos <ygor.vs.code@gmail.com>
2025-03-16 18:52:52 -0300

    aula015

---
+++ JavaScript/aulas/aula015/script01.js
@@ -0,0 +1,11 @@
+console.log('Início do Programa')
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(`${i} é Par.`)
+    } else {
+        console.log(`${i} é Impar.`)
+    }
+}
+
+console.log('Fim do Programa')
